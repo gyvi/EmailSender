5186e0fe02442ec1f136717d3b584f0a52f6484c
gyviktor <gyvi5150@gmail.com>
2017-09-21 00:22:39 +0200

    Fix (most) phpmd errors and refactor DateTime static calls

---
+++ src/EmailSender/Core/Scalar/Application/Factory/DateTimeFactory.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace EmailSender\Core\Factory;
+
+use EmailSender\Core\Scalar\Application\ValueObject\DateTime\DateTime;
+use EmailSender\Core\Scalar\Application\ValueObject\DateTime\Date;
+use EmailSender\Core\Scalar\Application\ValueObject\DateTime\Year;
+use EmailSender\Core\Scalar\Application\ValueObject\DateTime\Month;
+use EmailSender\Core\Scalar\Application\ValueObject\DateTime\Day;
+use EmailSender\Core\Scalar\Application\ValueObject\DateTime\Time;
+use EmailSender\Core\Scalar\Application\ValueObject\DateTime\Hour;
+use EmailSender\Core\Scalar\Application\ValueObject\DateTime\Minute;
+use EmailSender\Core\Scalar\Application\ValueObject\DateTime\Second;
+
+/**
+ * Class DateTimeFactory
+ *
+ * @package EmailSender\Core
+ */
+class DateTimeFactory
+{
+    /**
+     * @param int $year
+     * @param int $month
+     * @param int $day
+     * @param int $hour
+     * @param int $minute
+     * @param int $second
+     *
+     * @return \EmailSender\Core\Scalar\Application\ValueObject\DateTime\DateTime
+     */
+    public function buildFromNative(int $year, int $month, int $day, int $hour, int $minute, int $second): DateTime
+    {
+        return new DateTime(
+            new Date(
+                new Year($year),
+                new Month($month),
+                new Day($day)
+            ),
+            new Time(
+                new Hour($hour),
+                new Minute($minute),
+                new Second($second)
+            )
+        );
+    }
+
+    /**
+     * @param \DateTime $dateTime
+     *
+     * @return \EmailSender\Core\Scalar\Application\ValueObject\DateTime\DateTime
+     */
+    public function buildFromDateTime(\DateTime $dateTime): DateTime
+    {
+        return new DateTime(
+            new Date(
+                new Year(intval($dateTime->format('Y'))),
+                new Month(intval($dateTime->format('n'))),
+                new Day(intval($dateTime->format('j')))
+            ),
+            new Time(
+                new Hour(intval($dateTime->format('G'))),
+                new Minute(intval($dateTime->format('i'))),
+                new Second(intval($dateTime->format('s')))
+            )
+        );
+    }
+}
